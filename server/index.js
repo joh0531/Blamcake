d9585906a2921de8835ac3537aaff9a1466b9ab9
Jewon Oh <joh0531@gmail.com>
2018-09-25 13:27:50 -0400

    added route to serve index.html

--- server/index.js
+++ server/index.js
@@ -2,4 +2,7 @@ const app = require('express')()
 
 const port = process.env.PORT || 5000
 
+if (process.env.NODE_ENV === 'production')
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
 app.listen(port, () => console.log(`server running on port ${port}`))
