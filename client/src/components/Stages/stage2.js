42ec7200ef09834231afca1cf24d10886c5cfc9e
Matthew Phelps <matthew.130pm@gmail.com>
2018-09-27 14:45:29 -0400

    Adding imports to stage2

--- client/src/components/Stages/stage2.js
+++ client/src/components/Stages/stage2.js
@@ -1,3 +1,6 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
 
-export default () => <h1>STAGE 2</h1>
+// export default () => <h1>STAGE 2</h1>
+export default withStyles(theme)
