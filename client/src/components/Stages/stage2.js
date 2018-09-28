d51bffc6b6964b1f5e9efd151a7285912b076cb3
Jewon Oh <joh0531@gmail.com>
2018-09-28 09:41:18 -0400

    updated format of stage2

--- client/src/components/Stages/stage2.js
+++ client/src/components/Stages/stage2.js
@@ -4,29 +4,33 @@ import { Paper, Typography } from '@material-ui/core'
 
 export default withStyles(theme => ({
 	general: {
-		marginTop: 60,
-		marginBottom: 100,
-		marginLeft: 100,
-		marginRight: 100
-	},
-	Paper: {
-		padding: 25,
-		margin: 15
+		marginTop: theme.spacing.unit * 7,
+		marginBottom: theme.spacing.unit * 12,
+		marginLeft: theme.spacing.unit * 12,
+		marginRight: theme.spacing.unit * 12
 	},
 	indented: {
-		marginLeft: 45
+		marginLeft: theme.spacing.unit * 6
+	},
+	paper: {
+		padding: theme.spacing.unit * 3,
+		margin: theme.spacing.unit * 2
+	},
+	title: {
+		marginTop: 0
 	}
 }))(({ classes }) => (
 	<div className={classes.general}>
-		<Typography align="center" style={{ marginTop: 0 }}variant="display2">
-			Blamcake - Development Plan 
+		<Typography align="center" className={classes.title} variant="display2">
+			Blamcake - Development Plan
 		</Typography>
 		<Typography align="center" variant="display1">
-			DataBase Project Fall 2018 - Stage 2	
+			DataBase Project Fall 2018 - Stage 2
 		</Typography>
-
-		<Paper className={classes.Paper}>
-			<Typography variant="headline" gutterBottom>1. Relational Schema</Typography>	
+		<Paper className={classes.paper}>
+			<Typography variant="headline">
+				1. Relational Schema
+			</Typography>
 			{[
 				'Events(ID, all_day, created_at, location, start_at, end_at, content, title, image, url, isFeatured)',
 				'NDEvent(ID, all_day, created_at, location, start_at, end_at, content, title, image, url, isFeatured)',
@@ -35,37 +39,80 @@ export default withStyles(theme => ({
 				'Calendar(CATEGORY, length)',
 				'NotificationPreferences(EMAIL, timeofday, regularity)',
 			].map((bullet, i) => (
-				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`• ${bullet}`}</Typography>
+				<Typography
+					className={classes.indented}
+					color="inherit"
+					key={i}
+					variant="caption"
+				>
+					{`• ${bullet}`}
+				</Typography>
 			))}
 		</Paper>
-		<Paper className={classes.Paper}>
-			<Typography variant="headline" gutterBottom>2. Final Choice of Database and Software Platforms</Typography>	
+		<Paper className={classes.paper}>
+			<Typography variant="headline">
+				2. Final Choice of Database and Software Platforms
+			</Typography>
 			{[
-				'a. MongoDB - Database', 'b. Express.js - Framework', 'c. React.js - Front end',
-				'd. Node.js - Back end','e. Digital Ocean - Cloud Service',
+				'a. MongoDB - Database',
+				'b. Express.js - Framework',
+				'c. React.js - Front end',
+				'd. Node.js - Back end',
+				'e. Digital Ocean - Cloud Service'
 			].map((bullet, i) => (
-				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+				<Typography
+					className={classes.indented}
+					color="inherit"
+					key={i}
+					variant="caption"
+				>
+					{`${bullet}`}
+				</Typography>
 			))}
 		</Paper>
-		<Paper className={classes.Paper}>
-			<Typography variant="headline" gutterBottom>3. Where Our Data Comes From</Typography>	
+		<Paper className={classes.paper}>
+			<Typography variant="headline">
+				3. Where Our Data Comes From
+			</Typography>
 			{[
-				'a. Notre Dame Events API', 'b. User Submitted Events', 'c. User Submitted Personal Information',
+				'a. Notre Dame Events API',
+				'b. User Submitted Events',
+				'c. User Submitted Personal Information'
 			].map((bullet, i) => (
-				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
-			))}
-		</Paper>		
-		<Paper className={classes.Paper}>
-			<Typography variant="headline" gutterBottom>4. Labor Division</Typography>	
-			{[
-				'a. Jewon Oh: Back end (Database)', 'b. Paul Kwak: Back end (Server/API)',
-				'c. Matthew Phelps: Front end (UI/Responsiveness)', 'd. Maru (Eugene) Choi: Front end (UX/Design)',
-			].map((bullet, i) => (
-				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+				<Typography
+					className={classes.indented}
+					color="inherit"
+					key={i}
+					variant="caption"
+				>
+					{`${bullet}`}
+				</Typography>
 			))}
 		</Paper>
-		<Paper className={classes.Paper}>
-			<Typography variant="headline" gutterBottom>5. Project Milestone</Typography>	
+		<Paper className={classes.paper}>
+			<Typography variant="headline">
+				4. Labor Division
+			</Typography>
+			{[
+				'a. Jewon Oh: Back end (Database)',
+				'b. Paul Kwak: Back end (Server/API)',
+				'c. Matthew Phelps: Front end (UI/Responsiveness)',
+				'd. Maru (Eugene) Choi: Front end (UX/Design)'
+			].map((bullet, i) => (
+				<Typography
+					className={classes.indented}
+					color="inherit"
+					key={i}
+					variant="caption"
+				>
+					{`${bullet}`}
+				</Typography>
+			))}
+		</Paper>
+		<Paper className={classes.paper}>
+			<Typography variant="headline">
+				5. Project Milestone
+			</Typography>
 			{[
 				'Week 1: Sketch-up/Design layout, setup Database (MongoDB), setup routes',
 				'Week 2: Server - send requests to ND API / querying (routes that allow database queries)',
@@ -73,9 +120,16 @@ export default withStyles(theme => ({
 				'Week 4: Fine tuning components in app, separation of concerns',
 				'Week 5: Initial demo polish ... Details:'
 			].map((bullet, i) => (
-				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+				<Typography
+					className={classes.indented}
+					color="inherit"
+					key={i}
+					variant="caption"
+				>
+					{`${bullet}`}
+				</Typography>
 			))}
-			<Typography className={classes.indented} gutterBottom>
+			<Typography className={classes.indented}>
 				{[
 					'Oct 31st Stage 3 complete',
 					'Show how to insert records into database',
@@ -83,7 +137,14 @@ export default withStyles(theme => ({
 					'Show how to update records',
 					'Show how to delete records',
 				].map((bullet, i) => (
-					<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${i+1}. ${bullet}`}</Typography>
+					<Typography
+						className={classes.indented}
+						color="inherit"
+						key={i}
+						variant="caption"
+					>
+						{`${i+1}. ${bullet}`}
+					</Typography>
 				))}
 			</Typography>
 			{[
@@ -91,9 +152,15 @@ export default withStyles(theme => ({
 				'Final Demo: December, last class/exam day',
 				'Final Step: Put application url in our project repo on dsg1 server',
 			].map((bullet, i) => (
-				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+				<Typography
+					className={classes.indented}
+					color="inherit"
+					key={i}
+					variant="caption"
+				>
+					{`${bullet}`}
+				</Typography>
 			))}
-
 		</Paper>
 	</div>
 ))
