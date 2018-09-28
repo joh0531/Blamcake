fb184b9542f287e3a442ad25e828f0ff188ad0fb
Matthew Phelps <matthew.130pm@gmail.com>
2018-09-28 02:03:33 -0400

    Stage 2 tab now displays each numbered question in own paper. q1 and q2 done

--- client/src/components/Stages/stage2.js
+++ client/src/components/Stages/stage2.js
@@ -8,7 +8,11 @@ export default withStyles(theme => ({
 		margin: 100,
 	},
 	Paper: {
-		padding: 25
+		padding: 25,
+		margin: 15
+	},
+	indented: {
+		marginLeft: 45
 	}
 }))(({ classes }) => (
 	<div className={classes.general}>
@@ -18,12 +22,33 @@ export default withStyles(theme => ({
 		<Typography align="center" variant="display1">
 			DataBase Project Fall 2018 - Stage 2	
 		</Typography>
-		<Paper>
-			<Typography variant="headline" className={classes.Paper} >1</Typography>	
-			<Typography variant="headline" className={classes.Paper} >2</Typography>	
-			<Typography variant="headline" className={classes.Paper} >3</Typography>	
-			<Typography variant="headline" className={classes.Paper} >3</Typography>	
-			<Typography variant="headline" className={classes.Paper} >3</Typography>	
+
+		<Paper className={classes.Paper}>
+			<Typography variant="headline" gutterBottom>1. Relational Schema</Typography>	
+			{[
+				'Events(ID, all_day, created_at, location, start_at, end_at, content, title, image, url, isFeatured)',
+				'NDEvent(ID, all_day, created_at, location, start_at, end_at, content, title, image, url, isFeatured)',
+				'UserEvent(ID, all_day, created_at, location, start_at, end_at, content, title, image, url, isFeatured, club, netid)',
+				'Users(NETID, password, email)',
+				'Calendar(CATEGORY, length)',
+				'NotificationPreferences(EMAIL, timeofday, regularity)',
+			].map((bullet, i) => (
+				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`• ${bullet}`}</Typography>
+			))}
 		</Paper>
+		<Paper className={classes.Paper}>
+			<Typography variant="headline" gutterBottom>2. Final Choice of Database and Software Platforms</Typography>	
+			{[
+				'a. MongoDB - Database',
+				'b. Express.js - Framework',
+				'c. React.js - Frontend',
+				'd. Node.js - Backend',
+				'e. Digital Ocean - Cloud Service',
+			].map((bullet, i) => (
+				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+			))}
+		</Paper>
+			
+			
 	</div>
 ))
