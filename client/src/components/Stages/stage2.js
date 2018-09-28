09672afa0959d0d49b37b1115696e3a79998b6ec
Matthew Phelps <matthew.130pm@gmail.com>
2018-09-28 02:04:50 -0400

    removed small previous jsx

--- client/src/components/Stages/stage2.js
+++ client/src/components/Stages/stage2.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { CssBaseline, Paper, Typography } from '@material-ui/core'
 
-// export default () => <h1>STAGE 2</h1>
 export default withStyles(theme => ({
 	general: {
 		margin: 100,
