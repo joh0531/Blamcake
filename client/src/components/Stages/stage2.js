35344d7545f6a9a1c4272a1e481ac346dcfff5e4
Matthew Phelps <matthew.130pm@gmail.com>
2018-09-28 02:42:39 -0400

    Added all questions for stage 2

--- client/src/components/Stages/stage2.js
+++ client/src/components/Stages/stage2.js
@@ -4,7 +4,10 @@ import { CssBaseline, Paper, Typography } from '@material-ui/core'
 
 export default withStyles(theme => ({
 	general: {
-		margin: 100,
+		marginTop: 60,
+		marginBottom: 100,
+		marginLeft: 100,
+		marginRight: 100
 	},
 	Paper: {
 		padding: 25,
@@ -15,7 +18,7 @@ export default withStyles(theme => ({
 	}
 }))(({ classes }) => (
 	<div className={classes.general}>
-		<Typography align="center" variant="display2">
+		<Typography align="center" style={{ marginTop: 0 }}variant="display2">
 			Blamcake - Development Plan 
 		</Typography>
 		<Typography align="center" variant="display1">
@@ -38,16 +41,59 @@ export default withStyles(theme => ({
 		<Paper className={classes.Paper}>
 			<Typography variant="headline" gutterBottom>2. Final Choice of Database and Software Platforms</Typography>	
 			{[
-				'a. MongoDB - Database',
-				'b. Express.js - Framework',
-				'c. React.js - Frontend',
-				'd. Node.js - Backend',
-				'e. Digital Ocean - Cloud Service',
+				'a. MongoDB - Database', 'b. Express.js - Framework', 'c. React.js - Front end',
+				'd. Node.js - Back end','e. Digital Ocean - Cloud Service',
 			].map((bullet, i) => (
 				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
 			))}
 		</Paper>
-			
-			
+		<Paper className={classes.Paper}>
+			<Typography variant="headline" gutterBottom>3. Where Our Data Comes From</Typography>	
+			{[
+				'a. Notre Dame Events API', 'b. User Submitted Events', 'c. User Submitted Personal Information',
+			].map((bullet, i) => (
+				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+			))}
+		</Paper>		
+		<Paper className={classes.Paper}>
+			<Typography variant="headline" gutterBottom>4. Labor Division</Typography>	
+			{[
+				'a. Jewon Oh: Back end (Database)', 'b. Paul Kwak: Back end (Server/API)',
+				'c. Matthew Phelps: Front end (UI/Responsiveness)', 'd. Maru (Eugene) Choi: Front end (UX/Design)',
+			].map((bullet, i) => (
+				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+			))}
+		</Paper>
+		<Paper className={classes.Paper}>
+			<Typography variant="headline" gutterBottom>5. Project Milestone</Typography>	
+			{[
+				'Week 1: Sketch-up/Design layout, setup Database (MongoDB), setup routes',
+				'Week 2: Server - send requests to ND API / querying (routes that allow database queries)',
+				'Week 3: Setting up components in app, focus on displaying events to user and allow searching keywords/UI for database interaction',
+				'Week 4: Fine tuning components in app, separation of concerns',
+				'Week 5: Initial demo polish ... Details:'
+			].map((bullet, i) => (
+				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+			))}
+			<Typography className={classes.indented} gutterBottom>
+				{[
+					'Oct 31st Stage 3 complete',
+					'Show how to insert records into database',
+					'Show at least one query that searches and lists the returned records',
+					'Show how to update records',
+					'Show how to delete records',
+				].map((bullet, i) => (
+					<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${i+1}. ${bullet}`}</Typography>
+				))}
+			</Typography>
+			{[
+				'Week 6-14: Clean up front-end, feature ideation, add advanced features',
+				'Final Demo: December, last class/exam day',
+				'Final Step: Put application url in our project repo on dsg1 server',
+			].map((bullet, i) => (
+				<Typography className={classes.indented} key={i} variant="caption" color="inherit" gutterBottom>{`${bullet}`}</Typography>
+			))}
+
+		</Paper>
 	</div>
 ))
