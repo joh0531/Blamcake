848795e3333d594b0d79511b123dc3a773986650
Matthew Phelps <matthew.130pm@gmail.com>
2018-09-28 02:54:54 -0400

    Removed unnecessary import cssbaseline. stage2 component works properly and displays our responses

--- client/src/components/Stages/stage2.js
+++ client/src/components/Stages/stage2.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import { CssBaseline, Paper, Typography } from '@material-ui/core'
+import { Paper, Typography } from '@material-ui/core'
 
 export default withStyles(theme => ({
 	general: {
