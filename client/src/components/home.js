fe6994cb841f32fff6992c513493b3198cd45c4b
Matthew Phelps <mphelps3@nd.edu>
2018-10-18 16:05:35 -0400

    Starting an initial design for home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,3 +1,16 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
 
-export default () => <h1>HOME!</h1>
+// export default withStyles() => <h1>HOME!</h1>
+
+
+export default withStyles(theme => ({
+    home: {
+		// margin: theme.spacing.unit * 3
+    }
+}))(({ classes }) => (
+    <div className={classes.home}>
+
+	</div> 
+))
