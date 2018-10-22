9f3ee52103191829d7e2883a5ec251e7657a6402
Matthew Phelps <mphelps3@nd.edu>
2018-10-22 10:26:49 -0400

    Formatting boxes

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,20 +1,49 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles'
-import { Typography } from '@material-ui/core'
+import { Typography, Grid, Paper } from '@material-ui/core'
 
 // export default withStyles() => <h1>HOME!</h1>
 
 export default withStyles(theme => ({
 	home: {
 		display: 'flex',
-		justifyContent: 'center',
 		alignItems: 'flex-end',
+		flexGrow: 1,
 	},
 	icon: {
 		margin: theme.spacing.unit * 2,
-	}
+	},
+	paper: {
+		margin: theme.spacing.unit * 2,
+		padding: theme.spacing.unit * 2,
+		width: 200,
+		height: 200,
+	},
+	gridcontainer: {
+		margin: theme.spacing.unit * 4,
+	},
 }))(({ classes }) => (
     <div className={classes.home}>
-
+		<Grid container className={classes.gridcontainer}> 
+			<Grid item xs>
+				<Paper className={classes.paper}>hi</Paper>
+			</Grid>
+			<Grid item xs>
+				<Paper className={classes.paper}>hi</Paper>
+			</Grid>
+			<Grid item xs>
+				<Paper className={classes.paper}>hi</Paper>
+			</Grid>
+			<Grid item xs>
+				<Paper className={classes.paper}>hi</Paper>
+			</Grid>
+			<Grid item xs>
+				<Paper className={classes.paper}>hi</Paper>
+			</Grid>
+			<Grid item xs>
+				<Paper className={classes.paper}>hi</Paper>
+			</Grid>
+		</Grid>
 	</div> 
 ))
