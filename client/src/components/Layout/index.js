1dcf285a76c1ef04dd5ede1d534b0fbcecfd7563
Matthew Phelps <mphelps3@nd.edu>
2018-10-24 10:36:00 -0400

    Reorganizing display component

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { CssBaseline } from '@material-ui/core'
 import Navbar from './navbar'
+import Display from './display'
 
 export default withStyles(theme => ({
     content: {
