44b17e6407cc3bedfb6e2d4b97477f9a53d8fdba
Paul Kwak <pkwak1@nd.edu>
2018-10-24 13:21:59 -0400

    update route all

--- server/routes/update.js
+++ server/routes/update.js
@@ -3,7 +3,30 @@ const jsonParser = require('body-parser').json()
 // const utils = require('../utils')
 const rp = require('request-promise');
 
-const topics = ['architecture', 'athletics'];
+const topics = [
+	'architecture',
+	'arts-and-entertainment',
+	'arts-and-letters',
+	'athletics',
+	'business',
+	'centers-and-institutes',
+	'engineering',
+	'global-affairs',
+	'graduate-school',
+	'health-and-recreation',
+	'hesburgh-libraries',
+	'law',
+	'lectures-and-conferences',
+	'research',
+	'official-academic-calendar',
+	'ongoing',
+	'open-to-the-public',
+	'privately-sponsored-events',
+	'religious-and-spiritual',
+	'science',
+	'service',
+	'student-life'
+];
 
 router.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
@@ -13,39 +36,31 @@ router.use((req, res, next) => {
 
 router.get('/', jsonParser, (req, res) => {
 	const events = [];
-	topics.forEach( element => {
-		const options = {
-	    	// uri: 'https://events.nd.edu/events/calendar/athletics.json',
-			uri: `https://events.nd.edu/events/calendar/${element}.json`,
-	    	json: true, // Automatically parses the JSON string in the response
-			resolveWithFullResponse: true
-		};
-		console.log(element);
-		rp(options)
-		// ({}) obj destructuring - picking from api obj - elements array
-	    	.then(({elements}) => {
-	        	// console.log('User has %d events', events.length);
-				// res.send(e[elements]);
-				// events.append(elements);
-				console.log(elements);
-				elements.forEach( el => {
-					events.push(el);
-				})
-	    	});
-	    	// .catch(err => {
-	        // 	// API call failed...
-			// 	res.send('failed to get events api');
-	    	// });
+	new Promise((resolve, reject) => {
+		topics.forEach((element, i) => {
+			const options = {
+		    	// uri: 'https://events.nd.edu/events/calendar/athletics.json',
+				uri: `https://events.nd.edu/events/calendar/${element}.json?upcoming`,
+		    	json: true, // Automatically parses the JSON string in the response
+				// resolveWithFullResponse: true
+			};
+			console.log(element);
+			rp(options)
+			// ({}) obj destructuring - picking from api obj - elements array
+		    	.then(({elements}) => {
+					console.log(elements);
+					elements.forEach( el => {
+						events.push(el);
+					})
+					// forces for each loop to end on last topic
+					if(i === topics.length - 1) {
+						resolve();
+					}
+		    	});
+		})
+	}).then(() => {
+		res.send(events);
 	})
-	res.send(events);
-})
-
-router.post('/', jsonParser, (req, res) => {
-    // utils.makePDF(req.body.id)
-    //     .then(([pdf]) => utils.sendEmail(req.body.email, pdf))
-    //     .then(() => res.send('email sent!'))
-    //     .catch(error => res.send(error))
-
 })
 
 module.exports = router
