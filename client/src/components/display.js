9653845bce131e95239c359c2e7a76861a0b9d22
Matthew Phelps <mphelps3@nd.edu>
2018-10-24 21:34:27 -0400

    Pretty cards are pretty! Next steps: get default images to show. the dome maybe?

--- client/src/components/display.js
+++ client/src/components/display.js
@@ -1,19 +1,25 @@
 import React, { Fragment } from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import { Card, CardActions, Button, Typography } from '@material-ui/core'
+import { Card, CardActions, Button, Typography, Grid } from '@material-ui/core'
 import { CardContent, CardMedia, CardActionArea } from '@material-ui/core'
 import { purple } from '@material-ui/core/colors'
-//import LocationOn from 'material-ui-icons/LocationOn'
-import events from './content'
+import LocationOn from '@material-ui/icons/LocationOn'
+import events from './mockevents'
 
 export default withStyles(theme => ({
 	card: {
 		margin: theme.spacing.unit * 2,
+		minWidth: theme.spacing.unit * 60,
+		padding: theme.spacing.unit * 1.5,
 	},
-	pretty: {
-		color: purple[500]
-	}
-
+	location: {
+		color: purple[500],
+		fontSize: 14,
+	},
+	icon: {
+		marginRight: theme.spacing.unit * 1.5,
+		marginTop: theme.spacing.unit * 1.5,
+	},
 	//paper: {
 	//	margin: theme.spacing.unit * 0,
 	//	padding: theme.spacing.unit * 4,
@@ -27,7 +33,7 @@ export default withStyles(theme => ({
 	//	margin: theme.spacing.unit * 0,
 	//},
 }))(({ classes }) => (
-	<Fragment>
+	<Grid container justify="center"> 
 		{events.map(({ title, content, location }) => (
 		<Card className={classes.card}>
 			<CardActionArea>
@@ -35,25 +41,30 @@ export default withStyles(theme => ({
 				</CardMedia>
 				<CardContent>
 					<Typography i
-						variant="subtitle" 
-						component="h2"
+						variant="h4" 
 						color="primary"
 						gutterBottom
 					>
 						{ title }
 					</Typography>
-					<Typography variant="h5" component="h4">
+					<Typography variant="subtitle1">
 						{ content }
 					</Typography>
-					<Typography variant="h6" component="h5"
-						className={classes.pretty}>
+					<Typography 
+						variant="overline" 
+						className={classes.location}
+					>
+						<LocationOn 
+							className={classes.icon} 
+							fontSize="small"
+						/>
 						Location : { location }
 					</Typography>
 				</CardContent>
 			</CardActionArea>
 		</Card>
 		))}
-	</Fragment>
+	</Grid>
 	// <Grid container> 
 	// 	<Grid item sm>
 	// 		<Grid container className={classes.gridcontainer}> 
