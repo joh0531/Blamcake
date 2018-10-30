3d36ed980ba50f3d2ba89ea6c3126ba5b8c1adf7
Matthew Phelps <mphelps3@nd.edu>
2018-10-30 14:47:46 -0400

    Starting to add redirect for interest form

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Provider } from './context'
 import Layout from './Layout'
 import Home from './Home'
 import { Stage1, Stage2 } from './Stages'
 
-export default () => (
-    <BrowserRouter>
-        <Layout>
-            <Switch>
-				<Route exact component={Home} path="/" />
-                <Route component={Stage1} path="/stage1" />
-                <Route component={Stage2} path="/stage2" />
-            </Switch>
-        </Layout>
-    </BrowserRouter>
-)
+export default class extends Component {
+	state = {
+		interestFormComplete: false,
+	}
+	render() {
+		return (
+			<BrowserRouter>
+				<Provider value={{ state: this.state }}>
+					<Layout>
+						<Switch>
+							<Route exact component={Home} path="/" />
+							<Route component={Stage1} path="/stage1" />
+							<Route component={Stage2} path="/stage2" />
+						</Switch>
+					</Layout>
+				</Provider>
+			</BrowserRouter>
+		)
+	}
+}
