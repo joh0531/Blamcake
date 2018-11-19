3a496fbe1df0e75ce6fef56f5ca04e5d22ad66ec
Jewon Oh <joh0531@gmail.com>
2018-11-19 16:00:55 -0500

    deleted unnecessary import

--- server/routes/editEvent.js
+++ server/routes/editEvent.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const jsonParser = require('body-parser').json()
-const rp = require('request-promise');
-const Event = require('../models/Event');
+const Event = require('../models/Event')
 
 router.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
